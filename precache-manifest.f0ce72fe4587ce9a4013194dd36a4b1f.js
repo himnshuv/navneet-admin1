6ced96895766f3dcdc4b660a53380a300a68fe1f
beeonline1 <himanshu@beeonline.co.in>
2020-07-29 23:38:34 +0530

    Updates

---
+++ precache-manifest.f0ce72fe4587ce9a4013194dd36a4b1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6bac3c84e15448165b27bf8767e258",
+    "url": "/navneet-admin1/index.html"
+  },
+  {
+    "revision": "e7ba1c707197a2ec5466",
+    "url": "/navneet-admin1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90155138de0ffaae011f",
+    "url": "/navneet-admin1/static/js/2.5db110a2.chunk.js"
+  },
+  {
+    "revision": "2b37d755a7ed4be2871d98cfe7c3a560",
+    "url": "/navneet-admin1/static/js/2.5db110a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7ba1c707197a2ec5466",
+    "url": "/navneet-admin1/static/js/main.b71f06bf.chunk.js"
+  },
+  {
+    "revision": "b130739970f729a419e5",
+    "url": "/navneet-admin1/static/js/runtime-main.62d5b53b.js"
+  },
+  {
+    "revision": "6c8c7e46c270c13c05ac973bfcff0464",
+    "url": "/navneet-admin1/static/media/logo.6c8c7e46.png"
+  }
+]);
